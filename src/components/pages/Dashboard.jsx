a8316e1de31dc68a141bae20ec8420a86c3589d7
OM Soni <omsoni1223@gmail.com>
2024-07-14 09:58:50 +0530

    dashboard changes

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -8,8 +8,7 @@ export default function Dashboard() {
     <div className="flex">
       <Sidebar />
       <div className="m-3 md:m-10 w-[82%] rounded-md">
-        <p className="font-bold text-xl">👋 {user?.gymName}</p>{" "}
-        <p className="ml-8">Welcome to FitAdmin !</p>
+        
       </div>
     </div>
   );
