487e3c7ec942c4975460de3012516c1129751984
JayminGajera <jaymingajera2332@gmail.com>
2024-07-14 15:58:43 +0530

    book add funtinality dobe

--- src/components/pages/Inventory.jsx
+++ src/components/pages/Inventory.jsx
@@ -3,10 +3,12 @@ import Sidebar from "../Sidebar/sidebar";
 import axios from "axios";
 import { FaArrowLeft } from "react-icons/fa6";
 import { useNavigate } from "react-router-dom";
+import { Button } from "../ui/button";
 
 const Inventory = () => {
   const [books, setBooks] = useState([]);
-  const [serach,setSearch] = useState("")
+  const [search, setSearch] = useState("");
+  const [isbn, setIsbn] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -26,16 +28,51 @@ const Inventory = () => {
     }
   };
 
+  const filteredBooks = books.filter((book) =>
+    book.title.toLowerCase().includes(search.toLowerCase())
+  );
+
+
+  const handleAddBook =async() => {
+try {
+  const addedbook =await axios.post(`http://192.168.116.226:8055/books/addBookFromApi`,{
+    isbn
+  });
+
+  // if(addedbook?.)
+  console.log("book ",addedbook)
+
+  if(!!addedbook){
+    toast("Book added succesfully")
+  }else{
+    toast("book not found with this isbn")
+  }
+} catch (error) {
+  console.log("Error while add book")
+}
+  }
   return (
     <div className="w-full px-10 flex">
-    <Sidebar/>
-     
+      <Sidebar />
       <div className="text-black px-4">
-      <input onChange={(e) => setSearch(e.target.value)} placeholder="serach book" className="border rounded-md p-2 mt-2" type="text" />
-        {books?.map((book, index) => (
+        <input
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Search book"
+          className="border rounded-md p-2 mt-2 mr-2"
+          type="text"
+        />
+
+        <input
+          onChange={(e) => setIsbn(e.target.value)}
+          placeholder="Enter Book ISBN"
+          className="border rounded-md p-2 mt-2 mr-2"
+          type="text"
+        />
+        <Button onClick={handleAddBook}>Add Book</Button>
+        {filteredBooks.map((book, index) => (
           <div
             key={index}
-            className="border rounded-md p-4 flex flex-col-3 items-center mt-2 "
+            className="border rounded-md p-4 flex flex-col-3 items-center mt-2"
           >
             {book?.image ? (
               <img
